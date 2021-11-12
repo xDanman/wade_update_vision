850c47904205d240ea9d4fa62609cfa9c45eae60
DANIEL GUZMAN <dfguzman1@uc.cl>
2021-11-12 16:33:30 -0300

    credits

--- mrcnn/model.py
+++ mrcnn/model.py
@@ -6,7 +6,7 @@ Copyright (c) 2017 Matterport, Inc.
 Licensed under the MIT License (see LICENSE for details)
 Written by Waleed Abdulla
 """
-							
+# https://github.com/leekunhee/Mask_RCNN/blob/master/mrcnn/model.py				
 
 import os
 			 
